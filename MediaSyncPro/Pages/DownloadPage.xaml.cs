a76cae90fc87241f3a7fc408a3b616668b041c96
J0nathan550 <ihudokormov@gmail.com>
2024-02-05 13:37:26 +0100

    Updated the information and added statistics.

--- MediaSyncPro/Pages/DownloadPage.xaml.cs
+++ MediaSyncPro/Pages/DownloadPage.xaml.cs
@@ -152,15 +152,21 @@ public partial class DownloadPage : ContentPage
     {
         try
         {
+            int completed = 0, failed = 0;
             foreach (var track in tracks)
             {
                 Task task = Task.Run(async () =>
                 {
+                    await Task.Delay(20);
                     string? youTubeID = await spotifyClient.Tracks.GetYoutubeIdAsync(track.Id, token);
                     if (youTubeID != null)
                     {
                         try
                         {
+                            Dispatcher.Dispatch(() =>
+                            {
+                                downloadInformationStackLayout.Children.Add(new Label() { Text = $"Download started for: {track.Title}, NOTE: Videos or Audio from YouTube or Spotify can sometimes not install, due internet issues, or 'missing' in the YouTube or Spotify for some reason." });
+                            });
                             string path = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic) + "/MediaSync Pro";
                             if (!Directory.Exists(path)) Directory.CreateDirectory(path);
                             var streamManifest = await youtubeClient.Videos.Streams.GetManifestAsync("https://youtube.com/watch?v=" + youTubeID, token);
@@ -168,29 +174,36 @@ public partial class DownloadPage : ContentPage
                             await youtubeClient.Videos.Streams.DownloadAsync(streamInfo, path + "/" + track.Artists[0].Name + "-" + track.Title + $".{streamInfo.Container}", null, token);
                             Dispatcher.Dispatch(() =>
                             {
-                                downloadInformationStackLayout.Children.Add(new Label() { Text = $"Download completed: {track.Title}" });
+                                downloadInformationStackLayout.Children.Add(new Label() { Text = $"Download completed: {track.Title}!", TextColor = new Color(0, 255, 0, 255) });
                             });
+                            completed++;
                         }
                         catch (Exception e)
                         {
                             Trace.WriteLine(e.Message);
                             Dispatcher.Dispatch(() =>
                             {
-                                downloadInformationStackLayout.Children.Add(new Label() { Text = $"Downloading failed: {track.Title}" });
+                                downloadInformationStackLayout.Children.Add(new Label() { Text = $"Downloading failed for: {track.Title}, due internet issues, or 'missing' in Spotify. Try again, if that didn't work, possibly that track you are looking for is missing at all.", TextColor = new Color(255, 0, 0, 255) });
                             });
+                            failed++;
                         }
                     }
                     else
                     {
                         Dispatcher.Dispatch(() =>
                         {
-                            downloadInformationStackLayout.Children.Add(new Label() { Text = $"Downloading failed: {track.Title}" });
+                            downloadInformationStackLayout.Children.Add(new Label() { Text = $"Downloading failed for: {track.Title}, due internet issues, or 'missing' in Spotify. Try again, if that didn't work, possibly that track you are looking for is missing at all.", TextColor = new Color(255, 0, 0, 255) });
                         });
+                        failed++;
                     }
                 }, token);
                 downloadTasks.Add(task);
             }
             await Task.WhenAll(downloadTasks);
+            Dispatcher.Dispatch(() =>
+            {
+                downloadInformationStackLayout.Children.Add(new Label() { Text = $"Downloading was finished! Installed: {completed}, Failed: {failed}.", TextColor = new Color(255, 255, 0, 255) });
+            });
             downloadTasks = [];
             Dispatcher.Dispatch(() =>
             {
@@ -222,12 +235,18 @@ public partial class DownloadPage : ContentPage
         {
             string path = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic) + "/MediaSync Pro";
             if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+            int completed = 0, failed = 0; 
             foreach (var video in videos)
             {
                 Task task = Task.Run(async () =>
                 {
+                    await Task.Delay(20);
                     try
                     {
+                        Dispatcher.Dispatch(() =>
+                        {
+                            downloadInformationStackLayout.Children.Add(new Label() { Text = $"Download started for: {video.Title}, NOTE: Videos or Audio from YouTube or Spotify can sometimes not install, due internet issues, or 'missing' in the YouTube or Spotify for some reason." });
+                        });
                         string path = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic) + "/MediaSync Pro";
                         if (!Directory.Exists(path)) Directory.CreateDirectory(path);
                         var streamManifest = await youtubeClient.Videos.Streams.GetManifestAsync("https://youtube.com/watch?v=" + video.Id, token);
@@ -235,21 +254,27 @@ public partial class DownloadPage : ContentPage
                         await youtubeClient.Videos.Streams.DownloadAsync(streamInfo, path + "/" + video.Author + "-" + video.Title + $".{streamInfo.Container}", null, token);
                         Dispatcher.Dispatch(() =>
                         {
-                            downloadInformationStackLayout.Children.Add(new Label() { Text = $"Download completed: {video.Title}" });
+                            downloadInformationStackLayout.Children.Add(new Label() { Text = $"Download completed: {video.Title}", TextColor = new Color(0, 255, 0, 255) });
                         });
+                        completed++;
                     }
                     catch (Exception e)
                     {
                         Trace.WriteLine(e.Message);
                         Dispatcher.Dispatch(() =>
                         {
-                            downloadInformationStackLayout.Children.Add(new Label() { Text = $"Downloading failed: {video.Title}" });
+                            downloadInformationStackLayout.Children.Add(new Label() { Text = $"Downloading failed for: {video.Title}, due internet issues, or 'missing' in YouTube. Try again, if that didn't work, possibly that video or audio you are looking for is missing at all.", TextColor = new Color(255, 0, 0, 255) });
                         });
+                        failed++;
                     }
                 }, token);
                 downloadTasks.Add(task);
             }
             await Task.WhenAll(downloadTasks);
+            Dispatcher.Dispatch(() =>
+            {
+                downloadInformationStackLayout.Children.Add(new Label() { Text = $"Downloading was finished! Installed: {completed}, Failed: {failed}.", TextColor = new Color(255, 255, 0, 255) });
+            });
             downloadTasks = [];
             Dispatcher.Dispatch(() =>
             {
@@ -281,10 +306,16 @@ public partial class DownloadPage : ContentPage
         {
             string path = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic) + "/MediaSync Pro";
             if (!Directory.Exists(path)) Directory.CreateDirectory(path);
+            int completed = 0, failed = 0; 
             Task task = Task.Run(async () =>
             {
                 try
                 {
+                    await Task.Delay(20);
+                    Dispatcher.Dispatch(() =>
+                    {
+                        downloadInformationStackLayout.Children.Add(new Label() { Text = $"Download started for: {video.Title}, NOTE: Videos or Audio from YouTube or Spotify can sometimes not install, due internet issues, or 'missing' in the YouTube or Spotify for some reason." });
+                    });
                     string path = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic) + "/MediaSync Pro";
                     if (!Directory.Exists(path)) Directory.CreateDirectory(path);
                     var streamManifest = await youtubeClient.Videos.Streams.GetManifestAsync("https://youtube.com/watch?v=" + video.Id);
@@ -292,20 +323,26 @@ public partial class DownloadPage : ContentPage
                     await youtubeClient.Videos.Streams.DownloadAsync(streamInfo, path + "/" + video.Author + "-" + video.Title + $".{streamInfo.Container}", null, token);
                     Dispatcher.Dispatch(() =>
                     {
-                        downloadInformationStackLayout.Children.Add(new Label() { Text = $"Download completed: {video.Title}" });
+                        downloadInformationStackLayout.Children.Add(new Label() { Text = $"Download completed: {video.Title}", TextColor = new Color(0, 255, 0, 255) });
                     });
+                    completed++;
                 }
                 catch (Exception e)
                 {
                     Trace.WriteLine(e.Message);
                     Dispatcher.Dispatch(() =>
                     {
-                        downloadInformationStackLayout.Children.Add(new Label() { Text = $"Downloading failed: {video.Title}" });
+                        downloadInformationStackLayout.Children.Add(new Label() { Text = $"Downloading failed for: {video.Title}, due internet issues, or 'missing' in YouTube. Try again, if that didn't work, possibly that video or audio you are looking for is missing at all.", TextColor = new Color(255, 0, 0, 255) });
                     });
+                    failed++;
                 }
             }, token);
             downloadTasks.Add(task);
             await Task.WhenAll(downloadTasks);
+            Dispatcher.Dispatch(() =>
+            {
+                downloadInformationStackLayout.Children.Add(new Label() { Text = $"Downloading was finished! Installed: {completed}, Failed: {failed}.", TextColor = new Color(255, 255, 0, 255) });
+            });
             downloadTasks = [];
             Dispatcher.Dispatch(() =>
             {
